a223192d4ff6abff88acf60fd725f7dc6b0d50ff
Alex White <awhite214@gmail.com>
2021-04-20 19:11:16 -0400

    functions in index.js

--- index.js
+++ index.js
@@ -2,79 +2,78 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-    type: 'input',
-    message: "Enter a title for your project:",
-    name: 'title',
-    validate: function lengthCheck(text) {
-        if (text.length < 1) {
-            console.log ("Response required.")
-        } else {
-            return true
-        }
-    }
-  },
-  {
-    type: 'input',
-    message: "Describe your project, including your motivation for starting it, the problem you are solving, and what you learned:",
-    name: "description",
-    validate: function lengthCheck(text) {
-        if (text.length < 1) {
-            console.log ("Response required.")
-        } else {
-            return true
-        }
-    }
-  },
-  {
-    type: 'input',
-    message: "Include any installation requirements:",
-    name: 'installation',
-  },
-  {
-    type: 'input',
-    message: "Describe how the project is used:",
-    name: 'usage',
-    validate: function lengthCheck(text) {
-        if (text.length < 1) {
-            console.log ("Response required.")
-        } else {
-            return true
-        }
-    }
-  },
-  {
-    type: 'input',
-    message: "Name any contributors here:",
-    name: 'contributing',
-  },
-  {
-    type: 'list',
-    message: "Please select the license that your application is covered under.",
-    choices: ["Apache License 2.0", "Boost Software License 1.0", "GNU General Public License v3.0", "MIT License", "Mozilla Public License 2.0", "None"],
-    name: 'license',
-  },
-  {
-    type: 'input',
-    message: "What is your email?",
-    name: 'email',
-    validate: function validEmail(text) {
-        if (text.includes('@') && text.includes('.com')){
-          return true
-        } else {
-          console.log( "You must enter a valid email address." )
-        }
-    }
-  },
-];
+    'What is the name of your project?', 
+    'Describe your project, including your motivation for starting it, the problem you are solving, and what you learned', 
+    'What are the installation steps required to run the project?', 
+    'Describe the anticipated usage for the project', 
+    'Please add the full name of anyone that contributed to this project', 
+    'What license is your project is covered under?', 
+    'What is your email address?',
+    'Include a link to your deployed application if one exists'
+]
+
+const licenses = ['Apache license 2.0', 'Boost Software License 1.0', 'BSD 2-clause "Simplified" license', 'BSD 3-clause "New" or "Revised" license', 'Creative Commons Zero', 'Creative Commons Zero v1.0 Universal', 'Creative Commons Attribution 4.0', 'Creative Commons Attribution ShareAlike 4.0', 'Eclipse Public License 1.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU General Public License v3.0', 'GNU Lesser General Public License v3.0', 'IBM v1.0', 'ISC', 'MIT', 'Mozilla Public License 2.0', 'The Perl License', 'SIL Open Font License 1.1', 'The Unlicense', 'zLib License']
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generator(data), (err) => {
+        err ? console.log(err) : console.log(`Success!`)
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'contributors'
+        },
+        {
+            type: 'list',
+            message: questions[5],
+            name: 'license',
+            choices: licenses
+        },
+        {
+            type: 'input',
+            message: questions[6],
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: `${questions[8]} If not, enter 'N/a'!`,
+            name: 'applink',
+            default: 'N/a'
+        },
+    ])
+        .then((response) => {
+            writeToFile('customREADME.md', response)
+        })
+}
 
 // Function call to initialize app
 init();
